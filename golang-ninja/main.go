558135a40f58a05a1f08d19241e108d760ca1bcf
Bulat Rakhimov <ZvezdaFtoshopa@gmail.com>
2023-01-17 11:06:34 +0300

    test

--- golang-ninja/main.go
+++ golang-ninja/main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	var god = 2000
-	if god%400 == 0 || (god%4 == 0 && god%100 != 0) {
-		fmt.Println("wash god visokosniy suka")
-	} else {
-		fmt.Println("god nevisokosny suka")
+	i := 1
+	var tab1 string = "========================================"
+	fmt.Printf("%-18s%3s %-11s Price\n%v\n", "Spaceline", "Days", "Trip Type", tab1)
+	for i < 11 {
+		x := rand.Intn(3) + 1
+		var company string
+		switch x {
+		case 1:
+			company = "Virgin Galactic"
+		case 2:
+			company = "SpaceX"
+		case 3:
+			company = "Space Adventures"
+		}
+		y := rand.Intn(2) + 1
+		var trip string
+		switch y {
+		case 1:
+			trip = "One-way"
+		case 2:
+			trip = "Round-trip"
+		}
+		z := rand.Intn(15)
+		skorost := 16 + z                             //km/sec
+		stoimost := 36 + z                            //mln dollarrov
+		vremya := (62100000 / skorost) / 60 / 60 / 24 //days
+		if y == 2 {
+			vremya *= 2
+			stoimost *= 2
+		}
+		fmt.Printf("%-19v%3v %-11v $%4v\n", company, vremya, trip, stoimost)
+		i++
 	}
 }
